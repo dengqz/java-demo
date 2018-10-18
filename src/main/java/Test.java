279ce7f96edfd7d0f5915b91277efe0d336edffb
dunn.deng <dunn.deng@oyohotels.cn>
2018-10-18 10:40:18 +0800

    Initial commit

---
+++ src/main/java/Test.java
@@ -0,0 +1,11 @@
+/**
+ * Test
+ *
+ * @author dengqizhi
+ * @date 2018/10/18 10:21
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
